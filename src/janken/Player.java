ca0485b97d31fd4fd189743a1d082567cc3bcfcd
anquess <anquess@gmail.com>
2017-05-03 15:01:32 +0900

    Playerクラスにwincountを追加、notifyWinメソッドを追加、getWinCountを変更
    勝った数を覚えさせるため

--- src/janken/Player.java
+++ src/janken/Player.java
@@ -2,17 +2,20 @@ package janken;
 
 public class Player {
 	private String name;
+	private int	wincount;
 
 	public Player(String name) {
-		this.name = name;
+		this.name		= name;
 	}
 	@Override
 	public String toString(){
 		return this.name;
 	}
 	public int getWinCount() {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+		return this.wincount;
+	}
+	public void notifyWin() {
+		this.wincount++;
 	}
 
 }
--- tst/janken/PlayerTest.java
+++ tst/janken/PlayerTest.java
@@ -24,15 +24,27 @@ public class PlayerTest {
 	public void getWinCountTest1回も勝負していない場合は0(){
 		// Arrange
 		Player sut = new Player("せいのすけ");
-
 		// Actual
 		int actual		= sut.getWinCount();
 		int expected 	= 0;
 
 		// Assert
 		assertThat(actual,is(expected));
-
 	}
+
+	@Test
+	public void notifyWinTest勝ちを伝えてwinCountが1(){
+		// Arrange
+		Player sut = new Player("せいのすけ");
+		// Actual
+		sut.notifyWin();
+		int actual		= sut.getWinCount();
+		int expected 	= 1;
+
+		// Assert
+		assertThat(actual,is(expected));
+	}
+
 
 
 }
