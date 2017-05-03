ec90cd0e9bf97da8cb0fb683d04753019554d41c
anquess <anquess@gmail.com>
2017-05-03 13:56:54 +0900

    HandEnumのGUのjudgeメソッドを変更
    judgeTestGU_CHOKI()をパスするため

--- src/janken/HandEnum.java
+++ src/janken/HandEnum.java
@@ -2,7 +2,15 @@ package janken;
 
 public enum HandEnum{
 	GU{
-		public int judge(HandEnum hand){return 0;}
+		public int judge(HandEnum hand){
+			switch(hand){
+			case GU :
+				return 0;
+			case CHOKI:
+				return 1;
+			default:
+				return -1;}
+			}
 	},CHOKI,PAH;
 	public int judge(HandEnum hand){return 0;};
 
