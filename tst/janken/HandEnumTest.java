813ecabc222a1c7b4a4757b44d1b4483f0621bc0
anquess <anquess@gmail.com>
2017-05-03 14:05:08 +0900

    HandEnumTestをToStringTestクラスとJudgeTestクラスにネスト構造化
    整理(リファクタリング)

--- tst/janken/HandEnumTest.java
+++ tst/janken/HandEnumTest.java
@@ -5,71 +5,77 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class HandEnumTest {
 
-	@Test
-	public void toStringTestGU() {
-		// Arrange
-		HandEnum sut = GU;
+	public static class toStringTest{
+		@Test
+		public void ToStringTestGU() {
+			// Arrange
+			HandEnum sut = GU;
 
-		// Actual
-		String actual	= sut.toString();
-		String expected	= "GU";
+			// Actual
+			String actual	= sut.toString();
+			String expected	= "GU";
 
-		// Assert
-		assertThat(actual,is(expected));
+			// Assert
+			assertThat(actual,is(expected));
+		}
+
+		@Test
+		public void toStringTestCHOKI() {
+			// Arrange
+			HandEnum sut = CHOKI;
+
+			// Actual
+			String actual	= sut.toString();
+			String expected	= "CHOKI";
+
+			// Assert
+			assertThat(actual,is(expected));
+		}
+		@Test
+		public void toStringTestPAH() {
+			// Arrange
+			HandEnum sut = PAH;
+
+			// Actual
+			String actual	= sut.toString();
+			String expected	= "PAH";
+
+			// Assert
+			assertThat(actual,is(expected));
+		}
 	}
 
-	@Test
-	public void toStringTestCHOKI() {
-		// Arrange
-		HandEnum sut = CHOKI;
+	public static class JudgeTest{
+		@Test
+		public void judgeTestGU_GU(){
+			// Arrange
+			HandEnum sut = GU;
 
-		// Actual
-		String actual	= sut.toString();
-		String expected	= "CHOKI";
+			// Actual
+			int actual		= sut.judge(GU);
+			int expected	= 0;
 
-		// Assert
-		assertThat(actual,is(expected));
+			// Assert
+			assertThat(actual,is(expected));
+		}
+		@Test
+		public void judgeTestGU_CHOKI(){
+			// Arrange
+			HandEnum sut = GU;
+
+			// Actual
+			int actual		= sut.judge(CHOKI);
+			int expected	= 1;
+
+			// Assert
+			assertThat(actual,is(expected));
+
+		}
 	}
-	@Test
-	public void toStringTestPAH() {
-		// Arrange
-		HandEnum sut = PAH;
-
-		// Actual
-		String actual	= sut.toString();
-		String expected	= "PAH";
-
-		// Assert
-		assertThat(actual,is(expected));
-	}
-
-	@Test
-	public void judgeTestGU_GU(){
-		// Arrange
-		HandEnum sut = GU;
-
-		// Actual
-		int actual		= sut.judge(GU);
-		int expected	= 0;
-
-		// Assert
-		assertThat(actual,is(expected));
-	}
-	@Test
-	public void judgeTestGU_CHOKI(){
-		// Arrange
-		HandEnum sut = GU;
-
-		// Actual
-		int actual		= sut.judge(CHOKI);
-		int expected	= 1;
-
-		// Assert
-		assertThat(actual,is(expected));
-
-	}
-
 }
