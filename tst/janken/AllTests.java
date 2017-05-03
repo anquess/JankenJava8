4969bd614ab8239a4a7d723dc40c83d107f44f33
anquess <anquess@gmail.com>
2017-05-03 15:29:21 +0900

    テストスイートAllTestを追加
    ちまちまやるのが面倒

---
+++ tst/janken/AllTests.java
@@ -0,0 +1,11 @@
+package janken;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ HandEnumTest.class, PlayerTest.class,})
+public class AllTests {
+
+}
